ebe27dc471d8810414f70e1a90ac0dfa3aa41ab0
Louise Brown <louise.brown@nottingham.ac.uk>
2024-11-19 17:30:12 +0000

    Added message

--- RobotWriter6SkeletonCode/main.c
+++ RobotWriter6SkeletonCode/main.c
@@ -35,6 +35,7 @@ int main()
     WaitForDollar();
 
     printf ("\nThe robot is now ready to draw\n");
+    printf( "Added comment");
 
         //These commands get the robot into 'ready to draw mode' and need to be sent before any writing commands
     sprintf (buffer, "G1 X0 Y0 F1000\n");
